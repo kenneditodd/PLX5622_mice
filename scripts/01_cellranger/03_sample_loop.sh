fff1439294d442537928c42c12f29bff749a8d01
kenneditodd <kenneditodd@gmail.com>
2023-10-27 19:52:13 -0500

    changed sample names

--- scripts/01_cellranger/03_sample_loop.sh
+++ scripts/01_cellranger/03_sample_loop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for sample in E3_2M_F E3_14M_F E3_2M_M E3_14M_M E4_2M_F E4_14M_F E4_2M_M E4_14M_M
+for sample in E3CF1 E3CF2 E3CM1 E3CM2 E3PF1 E3PF2 E3PM1 E3PM2 E4CF1 E4CF2 E4CM1 E4CM2 E4PF1 E4PF2 E4PM1 E4PM2
 do
-	qsub 02_count.sh $sample
+	sbatch 02_count.sh $sample
 done
 
