a0a01435337068bc5673beadc6ceadbf97a53c59
kenneditodd <kenneditodd@gmail.com>
2023-10-31 11:14:16 -0500

    change path

--- scripts/01_cellranger/04_get_web_summary.sh
+++ scripts/01_cellranger/04_get_web_summary.sh
@@ -3,6 +3,6 @@
 # extract webs summaries
 for sample in E3_2M_F E3_14M_F E3_2M_M E3_14M_M E4_2M_F E4_14M_F E4_2M_M E4_14M_M
 do
-	cd /research/labs/neurology/fryer/m214960/Da_Mesquita/count/$sample/outs
+	cd /research/labs/neurology/fryer/m214960/PLX5622_mice/counts/$sample/outs
 	cp web_summary.html ../../web_summaries/"$sample"_web_summary.html
 done
