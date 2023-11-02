c421d8e4e4fb30950be19455ce22e4e77c0024bf
kenneditodd <kenneditodd@gmail.com>
2023-11-02 12:15:02 -0500

    update paths and sample names

--- scripts/01_cellranger/05_get_metrics.R
+++ scripts/01_cellranger/05_get_metrics.R
@@ -2,17 +2,25 @@
 library(stringr)
 
 # file locations
-locations <- c("/research/labs/neurology/fryer/m214960/Da_Mesquita/count/E3_2M_F/outs/metrics_summary.csv",
-               "/research/labs/neurology/fryer/m214960/Da_Mesquita/count/E3_2M_M/outs/metrics_summary.csv",
-               "/research/labs/neurology/fryer/m214960/Da_Mesquita/count/E3_14M_F/outs/metrics_summary.csv",
-               "/research/labs/neurology/fryer/m214960/Da_Mesquita/count/E3_14M_M/outs/metrics_summary.csv",
-               "/research/labs/neurology/fryer/m214960/Da_Mesquita/count/E4_2M_F/outs/metrics_summary.csv",
-               "/research/labs/neurology/fryer/m214960/Da_Mesquita/count/E4_2M_M/outs/metrics_summary.csv",
-               "/research/labs/neurology/fryer/m214960/Da_Mesquita/count/E4_14M_F/outs/metrics_summary.csv",
-               "/research/labs/neurology/fryer/m214960/Da_Mesquita/count/E4_14M_M/outs/metrics_summary.csv")
+locations <- c("/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E3CF1/outs/metrics_summary.csv",
+               "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E3CF2/outs/metrics_summary.csv",
+               "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E3CM1/outs/metrics_summary.csv",
+               "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E3CM2/outs/metrics_summary.csv",
+               "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E3PF1/outs/metrics_summary.csv",
+               "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E3PF2/outs/metrics_summary.csv",
+               "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E3PM1/outs/metrics_summary.csv",
+               "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E3PM2/outs/metrics_summary.csv",
+               "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E4CF1/outs/metrics_summary.csv",
+               "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E4CF2/outs/metrics_summary.csv",
+               "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E4CM1/outs/metrics_summary.csv",
+               "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E4CM2/outs/metrics_summary.csv",
+               "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E4PF1/outs/metrics_summary.csv",
+               "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E4PF2/outs/metrics_summary.csv",
+               "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E4PM1/outs/metrics_summary.csv",
+               "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E4PM2/outs/metrics_summary.csv")
 
 # sample names
-names <- str_match(locations, "/research/labs/neurology/fryer/m214960/Da_Mesquita/count/(.+)/outs/metrics_summary.csv")[,2]
+names <- str_match(locations, "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/(.+)/outs/metrics_summary.csv")[,2]
 
 # initialize df and loop through files
 df <- data.frame()
@@ -35,7 +43,7 @@ c.names <- c("estimated_cells", "mean_reads", "median_genes", "number_reads",
 colnames(df) <- c.names
 
 write.table(df, 
-            "/research/labs/neurology/fryer/m214960/Da_Mesquita/count/web_summaries/overall_metrics.tsv",
+            "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/web_summaries/overall_metrics.tsv",
             sep = "\t",
             quote = FALSE)
 
